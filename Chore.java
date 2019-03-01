8022d44208dfdc7637655b5e28438e16c812e2f2
Moranmi <47155173+Moranmi@users.noreply.github.com>
2019-02-28 18:50:32 -0800

    Add files via upload
    Pretty much nothing

--- Chore.java
+++ Chore.java
@@ -1,5 +1,20 @@
 package mainPack;
 
-public class Chore {
+import java.util.HashMap;
 
+public class Chore {
+	
+	private int amount;
+	private int minutes;
+	private String days;
+	private HashMap map;
+	private String choreName;
+	
+	public Chore(String choreNameP, int amountP, int minP, String choreNameP) {
+		choreName = choreNameP;
+		amount = amountP;
+		minutes = minP;
+		choreName = choreNameP;
+		
+	}
 }
